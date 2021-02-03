e20d7d3410115d61590f3311f558305a77cf2344
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-03 14:14:09 -0800

    controls for search are in place

---
+++ src/controls/NewsSearchControls.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NewsSearchControls = ({ onChange }) => {
+
+};
+
+NewsSearchControls.PropTypes = {
+  searchText: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired 
+};
+
+export default NewsSearchControls;
