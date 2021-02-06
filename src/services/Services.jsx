f9929af11435819775b148c2958e91fe981d5986
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-06 15:02:01 -0800

    updated tests all snapshots working still need to add react test library test

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,7 +1,8 @@
+const fetch = require('node-fetch');
+
 export const getArticles = (searchText) => {
   // eslint-disable-next-line max-len
   return fetch(`http://newsapi.org/v2/everything?q=${searchText}&sortBy=popularity&apiKey=03729509fcfd44b896e903648f02f16a`)
     .then(res => res.json())
     .then(json => json.articles);
-
 };
